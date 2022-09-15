aa1fdd49c66c9042c71dff0d79bebdb83964be91
JouJou <joujou@pop-os.localdomain>
2022-09-14 21:10:30 -0300

    Adjust code for deploy on heroku

--- src/database/db.js
+++ src/database/db.js
@@ -2,7 +2,7 @@ import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 
 dotenv.config();
-const mongoClient = new MongoClient("mongodb://localhost:27017");
+const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
 
 mongoClient
