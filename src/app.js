a4491d0cbf328a4b280e8863a60f6d84e4a68a80
JouJou <joujou@pop-os.localdomain>
2022-09-15 23:00:30 -0300

    Adding routes

--- src/app.js
+++ src/app.js
@@ -8,6 +8,6 @@ app.use(express.json());
 
 app.use(router);
 
-app.listen(process.env.PORT, () => {
-  console.log("Server running on port " + process.env.PORT);
+app.listen(5000, () => {
+  console.log("Listen on 5000");
 });
